9a829fc121c70e8f8e1c08b5a80d2562e36a5f37
JD Fortune <jondfortune@gmail.com>
2022-11-04 16:14:48 -0500

    wrap up

--- programs/test_tictactoe.rb
+++ programs/test_tictactoe.rb
@@ -15,15 +15,21 @@ class Board
                 [:tr, :cm, :bl]]
 
   def initialize
-    @status = {tl: '7', tm: '8', tr: '9', cl: '4', cm: '5', cr: '6', bl: '1', bm: '2', br: '3'}
+    @status = {tl: Square.new(7), tm: Square.new(8), tr: Square.new(9), cl: Square.new(4), cm: Square.new(5), cr: Square.new(6), bl: Square.new(1), bm: Square.new(2), br: Square.new(3)}
   end
 
   def to_s
     puts ""
   end
 
-  def full
-    !@status.values.match?(/[1-9]/)
+  def full? 
+    !@status.none? do |key, square|
+      (1..9).include?(square.value)
+    end
+  end
+
+  def get_square(square)
+    status[square].value
   end
 
   def someone_won?(order)
@@ -45,8 +51,9 @@ class Board
 end
 
 class Square
-  def initialize
-    @status = nil
+  attr_accessor :value
+  def initialize(temp)
+    @value = temp
   end
 end
 
@@ -98,7 +105,11 @@ end
 class TTTGame
   include Displayable
 
-  attr_reader :player, :computer
+  attr_reader :player, :computer, :board
+
+  def initialize
+    @board = Board.new
+  end
 
   def display_welcome_message
     puts 'Welcome to Tic Tac Toe!'
@@ -109,18 +120,18 @@ class TTTGame
     puts 'Thanks for playing Tic Tac Toe! Goodbye'
   end
 
-  def display_board(brd)
+  def display_board
     puts ''
     puts '     |     |'
-    puts "  #{brd.status[:tl]}  |  #{brd.status[:tm]}  |  #{brd.status[:tr]}"
+    puts "  #{board.get_square(:tl)}  |  #{board.get_square(:tm)}  |  #{board.get_square(:tr)}"
     puts '     |     |'
     puts '-----+-----+-----'
     puts '     |     |'
-    puts "  #{brd.status[:cl]}  |  #{brd.status[:cm]}  |  #{brd.status[:cr]}"
+    puts "  #{board.get_square(:cl)}  |  #{board.get_square(:cm)}  |  #{board.get_square(:cr)}"
     puts '     |     |'
     puts '-----+-----+-----'
     puts '     |     |'
-    puts "  #{brd.status[:bl]}  |  #{brd.status[:bm]}  |  #{brd.status[:br]}"
+    puts "  #{board.get_square(:bl)}  |  #{board.get_square(:bm)}  |  #{board.get_square(:br)}"
     puts '     |     |'
     puts ''
   end
@@ -136,20 +147,25 @@ class TTTGame
     input[0] == 'p' ? player_order : player_order.reverse
   end
 
+  def someone_won?
+  end 
   def play
     clear_screen
     display_welcome_message
-    board = Board.new
     @player = Human.new
     puts player.name
     @computer = Computer.new
     puts computer.name
-    unless someone_won? || board.full? do
-      display_board(board)
+    display_board
+    unless someone_won? || board.full?
+      display_board
       play_order = set_player
+
       play_order.each do |player|
         player.move
         break if someone_won? || board.full?
+      end
+
     end
     # display_winner
     display_goodbye_message
@@ -158,3 +174,6 @@ end
 
 game = TTTGame.new
 game.play
+
+# game.display_board
+# p "#{Board.new.get_square(:tl)}"
